ad25d1b9151ece8ffd8b6d2b36db85170f8762dc
SysUtils <vadim.vinokurov99@yandex.ru>
2019-01-16 21:11:21 +0300

    fix

--- generator.php
+++ generator.php
@@ -5,65 +5,53 @@
 ** --------------------
 */
 
-
 require "memory.php";
 function readTheFile($path) {
     $handle       = fopen($path, 'r');
+    fgets($handle); // first line fix
     $handleOutput = fopen('output.txt', 'w');
-    fwrite($handleOutput, "MolID;Target;Ligand;Run;Pose;Rank;dG;VS;Atom;X;Y;Z;".PHP_EOL);
+    fwrite($handleOutput, "MolID;Target;Ligand;Run;Pose;Rank;dG;VS;Atom;X;Y;Z".PHP_EOL);
     $i=0;
-    $b=-1; // wish granted
+    $b=0;
     $atomsamount = 0;
+    $array = "";
+    $at = "";
+    $form = "";
+    $replaced = preg_replace('/ +/', ',', fgets($handle)); // maybe replace with strtr?
     while(!feof($handle)) {
-         $replaced = preg_replace('/\s\s+/', ',', fgets($handle)); // maybe replace with strtr?
-         foreach(explode('\x0A', $replaced) as $line) // "\n"
-         {
-           $c = strlen($line);
-           $parsedMolLine = explode(",", $line);
-           $test = strlen($line);
+        
+        foreach(explode('\x0A', $replaced) as $line) // "\n"
+        {
+            $c = strlen($line);
+            $parsedMolLine = explode(",", $line);
             switch ($c) {
-                case $c<=4:
+                case $c<=4: // atoms count
                     $i++;
                     $atomsamount += (int)$c;
                     break;
-                case $c>96:
-                    //Temporary fix of undefined offsets (line 34)
-                    if($test > 50)
-                    {
-                        $p = explode("-", $parsedMolLine[0]); // parsed from $parsedMolLine with "-"
-                        $b++;
-                        $form = $b.';'.$p[0].';'.$p[1].';'.$p[4].';'.$p[6].';'.$parsedMolLine[1].';'.$parsedMolLine[2].';'.$parsedMolLine[3].';';
-                        break;
-                    }
-                case ($c>20 && $c<90):
-                    //Temporary fix of undefined offsets (line 42)
-                    if($test != 0)
-                    {
-                        $ughjeez = trim($parsedMolLine[3]);
-                        $at   = $parsedMolLine[0].';'.$parsedMolLine[1].';'.$parsedMolLine[2].';'.$ughjeez;
-                        break;
-                    }
+                case $c>90: // moleculaa
+                    $p = explode("-", $parsedMolLine[0]); // parsed from $parsedMolLine with "-"
+                    $form = $b.';'.$p[0].';'.$p[1].';'.$p[4].';'.$p[6].';'.$parsedMolLine[1].';'.$parsedMolLine[2].';'.$parsedMolLine[3].';';
+                    $b++;
+                    break;
+                case ($c>20 && $c<90): //atom
+                    $ughjeez = trim($parsedMolLine[3]);
+                    $at   = $parsedMolLine[0].';'.$parsedMolLine[1].';'.$parsedMolLine[2].';'.$ughjeez;
+                    $array .= $form.$at.PHP_EOL;
+                    break;
             }
-
-            @$array .= $form.$at.';'.PHP_EOL; // this is a string ok very cool
-
-            $bytesize = strlen($array).PHP_EOL;
-            if($bytesize < 64) // fixing weird 3 strings on start of algo
-            {
-                unset($array);
-            }
-
+            
+            $bytesize = strlen($array);
             if($bytesize > 1000000) // unset array for less ram usage
             {
                 fwrite($handleOutput, $array);
-                unset($array, $bytesize);
+                $array = "";
             }
-           
-           yield $line;
-         }
+            yield $line;
+            $replaced = preg_replace('/ +/', ',', fgets($handle)); // maybe replace with strtr?
+        }
     }
-    fwrite($handleOutput, $array); // Saving last info into a file.
-
+    fwrite($handleOutput, $array);
     fclose($handle);
     fclose($handleOutput);
     echo "Amount of atoms:     ".$atomsamount,PHP_EOL;
